10efae1a6195ddb3b5837b4339001abc480d64b4
lakshay-0423 <blakshay162@gmail.com>
2025-07-04 09:25:02 +0530

    first code of lecture 3

---
+++ l3.cpp
@@ -0,0 +1,13 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"My name"<<endl<<endl<<"potato\n"<<endl<<"and"<<"IEAT" <<"Tomato";
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
